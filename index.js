27d9a991bb42145b80bb3646e1141044e12dd5f7
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-10-09 12:26:47 -0400

    how ethnic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const DOMSelectors = {
   header: document.querySelector("h1"),
   subHeader: document.querySelector("h3"),
+  button: document.querySelector("button"),
+  card: document.querySelector(".card"),
 };
-function buttonInformation() {}
