0315997965f7cf0587a1ad37eec4496d2e9fa02a
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-10-10 12:29:48 -0400

    im like hey whats up hello

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const DOMSelectors = {
   header: document.querySelector("h1"),
   subHeader: document.querySelector("h3"),
   button: document.querySelector("button"),
+  container: document.querySelector(".container"),
   card: document.querySelector(".card"),
+  text: document.getElementById("value"),
+  cardHeader: document.qyerySelector("cardHeader"),
 };
+function addElement(card) {
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    '<div class="card"><h2 class="header</div>'
+  );
+}
