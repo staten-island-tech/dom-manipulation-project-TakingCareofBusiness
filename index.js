f1aba1e449dc5e7349eb7a910e6c2f043a0bf5c4
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-10-15 08:13:12 -0400

    hm

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ const DOMSelectors = {
 let currentPikminName;
 let currentPikminColor;
 let currentPikminDescription;
+let currentPikminBase;
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   let currentPikminName = document.querySelector("#name").value;
   let currentPikminColor = document.querySelector("#color").value;
   let currentPikminDescription = document.querySelector("#description").value;
-  console.log(currentPikminName);
+  let currentPikminBase = document.querySelector("#type").value;
+
   DOMSelectors.container.insertAdjacentHTML(
     "beforeEnd",
-    `<div class="card"><h2 class="card-header">${currentPikminName}</h2><img src="BlankPikmin.png" alt="Blank Pikmin Base" class="card-image"><p>${currentPikminDescription}</p></div>`
+    `<div class="card"><h2 class="card-header">${currentPikminName}</h2><img src=PikminBase alt="Blank Pikmin Base" class="card-image"><p>${currentPikminDescription}</p></div>`
   );
 });
