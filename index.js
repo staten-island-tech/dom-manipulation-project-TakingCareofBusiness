ab13e9baf5dd2f8248ba7f05ae4c0e7793614967
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-10-14 21:39:06 -0400

    bruh moment

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const DOMSelectors = {
   header: document.querySelector("h1"),
   subHeader: document.querySelector("h3"),
   button: document.querySelector("button"),
-  container: document.querySelector(".container"),
+  container: document.querySelector(".card-container"),
   card: document.querySelector(".card"),
-  text: document.querySelector(".value"),
   cardHeader: document.querySelector("cardHeader"),
+  form: document.querySelector("form"),
 };
-function addElement(card) {
+let currentPikminName;
+let currentPikminColor;
+let currentPikminDescription;
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let currentPikminName = document.querySelector("#name").value;
+  let currentPikminColor = document.querySelector("#color").value;
+  let currentPikminDescription = document.querySelector("#description").value;
+  console.log(currentPikminName);
   DOMSelectors.container.insertAdjacentHTML(
-    "beforeend",
-    `<div class="card"><h2 class="header">${card}</h2></div>`
+    "beforeEnd",
+    `<div class="card"><h2 class="card-header">${currentPikminName}</h2><img src="BlankPikmin.png" alt="Blank Pikmin Base" class="card-image"><p>${currentPikminDescription}</p></div>`
   );
-}
-DOMSelectors.button.addEventListener("click", function (noClear) {
-  noClear.preventDefault();
-  addElement(DOMSelectors.text.value);
 });
