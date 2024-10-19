60dd57d014aecd7c7eb80b0aaf956ad5c832deb4
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-10-18 22:31:50 -0400

    I get it I get it I get it

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const DOMSelectors = {
   button: document.querySelector(".submit-button"),
   container: document.querySelector(".card-container"),
   card: document.querySelector(".card"),
-  cardHeader: document.querySelector(".cardHeader"),
+  cardHeader: document.querySelector(".cardHeader"), // Fixed selector
   form: document.querySelector("form"),
   typeOptions: document.querySelector(".dropdown-content"),
   colorInput: document.querySelector("#color"),
+  title: document.getElementById("name"),
+  color: document.getElementById("color"),
+  description: document.getElementById("description"),
 };
+
 const pikminBodies = [
   { name: "Blue", subname: "blue", image: "BluePikmin.png" },
   { name: "Yellow", subname: "yellow", image: "YellowPikmin.png" },
@@ -20,47 +24,50 @@ const pikminBodies = [
   { name: "Rock", subname: "rock", image: "RockPikmin.png" },
   { name: "White", subname: "white", image: "WhitePikmin.png" },
 ];
-let imageSelected = [];
-function typeSelect() {
-  DOMSelectors.typeOptions.addEventListener("click", function (event) {
-    let pikminType = event.target.className;
-    let selectedType;
-    pikminBodies
-      .filter((type) => type.name === pikminType)
-      .forEach((type) => (selectedType = type.image));
-    console.log(selectedType);
-    if (imageSelected.length !== 0) {
-      console.log(imageSelected[0]), (imageSelected.length = 0);
-    } else {
-      imageSelected.push(selectedType);
-    }
-  });
-}
-function createObject(currentPikminType) {
+let currentPikminType;
+function createObject() {
   DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     let currentPikminName = document.querySelector("#name").value;
     let currentBackgroundColor = DOMSelectors.colorInput.value;
     let currentPikminDescription = document.querySelector("#description").value;
-    console.log(currentPikminType);
     insertObject(
       currentBackgroundColor,
       currentPikminName,
       currentPikminType,
       currentPikminDescription
     );
+    clearPrompt();
   });
 }
+function typeSelect() {
+  DOMSelectors.typeOptions.addEventListener("click", function (event) {
+    let pikminType = event.target.className;
+    pikminBodies.forEach((type) => {
+      if (type.name === pikminType) {
+        currentPikminType = type.image;
+      }
+    });
+  });
+}
+function clearPrompt() {
+  DOMSelectors.title.value = "";
+  DOMSelectors.color.value = "";
+  DOMSelectors.description.value = "";
+}
+
+let idNumber = 0;
 function insertObject(
   currentBackgroundColor,
   currentPikminName,
   currentPikminType,
   currentPikminDescription
 ) {
+  idNumber += 1;
   DOMSelectors.container.insertAdjacentHTML(
     "beforeEnd",
-    `<div class="card" style="background-color: ${currentBackgroundColor};"><h2 class="card-header">${currentPikminName}</h2><img src=${currentPikminType} class="card-image"><p>${currentPikminDescription}</p></div>`
+    `<div class="card" style="background-color: ${currentBackgroundColor};" id="card-id-${idNumber}"><h2 class="card-header">${currentPikminName}</h2><img src=${currentPikminType} class="card-image"><p>${currentPikminDescription}</p><button class="delete-card" id="remove-button-${idNumber}">Remove</button></div>`
   );
 }
 typeSelect();
-createObject(currentPikminType);
+createObject();
